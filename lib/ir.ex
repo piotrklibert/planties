4651a248036efed08a13412170923447c201a8a1
Piotr Klibert <piotr.klibert@10clouds.com>
2016-03-27 22:07:25 +0200

    Forgot to compile during update...

--- lib/ir.ex
+++ lib/ir.ex
@@ -1,13 +1,11 @@
 defmodule Ir do
   @moduledoc """
   This module depends on correctly configured lirc.
-
-  TODO: add lirc-related things to Ansible deployment
   """
   use GenServer
 
   defmodule State do
-    defstruct port: nil, messages: [], listeners: []
+    defstruct port: nil, listeners: []
   end
 
   @global_name {:global, :ir}
@@ -26,22 +24,50 @@ defmodule Ir do
     GenServer.call @global_name, :get
   end
 
+  def subscribe(pid) do
+    GenServer.call @global_name, {:register, self()}
+  end
+
+
   # Server section
   # ----------------------------------------------------------------------------
 
   def init(state) do
-    port = Port.open {:spawn, "ircat myprog"}, line: 50
+    port = Port.open {:spawn, "ircat myprog"}, line: 10
+    IO.inspect [port, state]
     {:ok, %State{state | port: port}}
   end
 
-  def handle_call(_msg, _from, state) do
-    {:reply, state.messages, state}
+  def handle_call({:register, pid}, _from, state) do
+    IO.inspect state.listeners
+    {:reply, :ok, %State{state | listeners: [pid | state.listeners]}}
   end
 
   def handle_info({port, {:data, {_, msg}}}, state) do
-    {:noreply, %State{state | messages: [msg | state.messages]}}
+    for pid <- state.listeners do
+      send pid, {:ir, msg}
+    end
+    {:noreply, state}
   end
   def handle_info(_msg, state) do
+    {:noreply, state}
+  end
+end
+
+defmodule Ir.Mon do
+  use GenServer
+
+  def start_link() do
+    GenServer.start_link __MODULE__, nil, []
+  end
+
+  def init(nil) do
+    Ir.subscribe self()
+    {:ok, nil}
+  end
+
+  def handle_info({:ir, txt}, state) do
+    IO.inspect txt
     {:noreply, state}
   end
 end
